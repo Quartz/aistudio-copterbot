0911cba5e1109a6d4b112432fe19d1c24fbc0d3c
Jeremy B. Merrill <jmerrill@qz.com>
2019-03-19 16:46:19 -0400

    time zone fix for training data generator

--- generate_images_for_hand_classification.rb
+++ generate_images_for_hand_classification.rb
@@ -34,6 +34,7 @@ copters = {
   "N422PD" => "A50456",
   "N414PD" => "A4E445",
   "N23FH"  => "A206AC",
+  "N509PD" => "A65CA8",
 }
 SHINGLE_DURATION = 5 # minutes
 SHINGLE_MIN_POINTS = 10 # minimum points in a trajectory.
@@ -119,8 +120,8 @@ aircraft.each do |nnum, icao|
     shingled_trajectories.each do |traj_shingles|
         traj_shingles.each do |shingle|
             next if shingle.length == 0
-            shingle_start_time = shingle[-1]["parsed_time"].to_s.gsub(" -0500", '')
-            shingle_end_time = shingle[0]["parsed_time"].to_s.gsub(" -0500", '')
+            shingle_start_time = shingle[-1]["parsed_time"].to_s.gsub(/ -0\d00/, '')
+            shingle_end_time = shingle[0]["parsed_time"].to_s.gsub(/ -0\d00/, '')
             shingle_svg_fn = "hover_train_svg/#{shingle[0]["icao_hex"]}_#{shingle_start_time.gsub(/[ \:]/, '_')}_#{shingle_end_time.gsub(/[ \:]/, '_')}.svg"
             shingle_cmd = "node ../dump1090-mapper/mapify.js #{icao} #{nnum} '#{shingle_start_time}' '#{shingle_end_time}'"
             unless File.exists?(shingle_svg_fn) && File.new(shingle_svg_fn).size > 0
@@ -139,14 +140,16 @@ aircraft.each do |nnum, icao|
                     raise e 
                 end
             end
+            client_count = shingle.map{|row| row["client_id"]}.uniq.count
             shingles_csv << [shingle[0]["icao_hex"],
                     shingle_start_time,
                     shingle_end_time,
                     shingle_svg_fn, shingle_png_fn,
+                    client_count
                     ] + shingle.map{|pt| [pt["lat"].to_f.round(6), pt["lon"].to_f.round(6)]}
         end
     end
-
+    puts shingled_trajectories.map{|t| t.size }.reduce(&:+)
     # svg_fn = `MAXTIMEDIFF=#{10} /usr/local/bin/node #{File.dirname(__FILE__)}/../dump1090-mapper/mapify.js #{icao} #{nnum}`
     # svg_fn.strip!
     # metadata_fn = svg_fn.gsub(".svg", ".metadata.json")
