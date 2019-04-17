19cd02f400bc8a7d33d191630b5001ccb883ce68
Victoria Cabales <vcabales96@gmail.com>
2019-04-17 12:12:09 -0500

    python script to load pickled model

---
+++ classify_one_map.py
@@ -0,0 +1,13 @@
+from fastai.vision import *
+import sys
+
+def main():
+    learn = load_learner('./models')
+    img = open_image(sys.argv[1])
+    pred_class,pred_idx,outputs = learn.predict(img) 
+    # print (pred_class)
+    # print (pred_idx)
+    # print (outputs)
+    return pred_class
+
+main()
